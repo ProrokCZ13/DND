d392bcd464a114d4012827d375eeac26a60b3875
Vojtěch Černý <91047341+ProrokCZ13@users.noreply.github.com>
2021-12-14 15:04:12 +0100

    Update Main.java
    class stats

--- src/com/DND/Main.java
+++ src/com/DND/Main.java
@@ -18,13 +18,13 @@ public class Main {
         Maps maps = new Maps(player);
     }
 
-      // public static void Music() {
-      //     file = new File("src/com/DND/media/The Elder Scrolls IV - Oblivion Soundtrack - 04 Harvest Dawn.mp3");
-      //     music = new MP3Player(file);
-       //    while (true) {
-       //        music.play();
-       //    }
-      // }
+    //   public static void Music() {
+    //       file = new File("src/com/DND/media/The Elder Scrolls IV - Oblivion Soundtrack - 04 Harvest Dawn.mp3");
+    //       music = new MP3Player(file);
+    //       while (true) {
+    //           music.play();
+    //       }
+    //   }
 
     public static void chooseName() {
         System.out.print("WHAT IS YOUR NAME?: ");
@@ -33,33 +33,86 @@ public class Main {
     }
 
     public static void chooseClass() {
-        System.out.println("1 - CRUSADER ; 2 - BARBARIAN ; 3 - ASSASSIN; 4 - NECROMANCER; 5 - MAGE; 6 - HUNTER; 7 - PALADIN; 8 - HAM; 9 - CUM");
-        System.out.print("Choose your character type: ");
+        System.out.println("1 - CRUSADER");
+        System.out.println("HP: 120");
+        System.out.println("DMG: 40");
+        System.out.println("LUCK: 15");
+        System.out.println("CASH: 0");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println(" 2 - BARBARIAN");
+        System.out.println("HP: 100");
+        System.out.println("DMG: 20");
+        System.out.println("LUCK: 35");
+        System.out.println("CASH: 100");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("3 - ASSASSIN");
+        System.out.println("HP:75");
+        System.out.println("DMG:10");
+        System.out.println("LUCK:50");
+        System.out.println("CASH:225");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("4 - NECROMANCER");
+        System.out.println("HP:50");
+        System.out.println("DMG:75");
+        System.out.println("LUCK:10");
+        System.out.println("CASH:225");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("5 - MAGE");
+        System.out.println("HP:50");
+        System.out.println("DMG:70");
+        System.out.println("LUCK:10");
+        System.out.println("CASH:225");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("6 - HUNTER");
+        System.out.println("HP:45");
+        System.out.println("DMG:70");
+        System.out.println("LUCK:45");
+        System.out.println("CASH:225");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println(" 7 - PALADIN");
+        System.out.println("HP:100");
+        System.out.println("DMG:60");
+        System.out.println("LUCK:50");
+        System.out.println("CASH:100");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("8 - HAM");
+        System.out.println("HP:1");
+        System.out.println("DMG:1");
+        System.out.println("LUCK:100");
+        System.out.println("CASH:1000");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("9 - CUM");
+        System.out.println("HP:100");
+        System.out.println("DMG:100");
+        System.out.println("LUCK:0");
+        System.out.println("CASH:0");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.print("CHOOSE YOUR CLASS: ");
         int type = scanner.nextInt();
         if (type == 1) {
             player.setHealth(120);
             player.setDamage(40);
             player.setLuck(.15);
             player.setWallet(0);
-            System.out.println("You chose CRUSADER");
+            System.out.println("YOU CHOSE CRUSADER");
         } else if (type == 2) {
             player.setHealth(100);
             player.setDamage(20);
             player.setLuck(.35);
             player.setWallet(100);
-            System.out.println("You chose BARBARIAN");
+            System.out.println("YOU CHOSE BARBARIAN");
         } else if (type == 3) {
             player.setHealth(75);
             player.setDamage(10);
             player.setLuck(.50);
             player.setWallet(225);
-            System.out.println("You chose ASSASSIN");
+            System.out.println("YOU CHOSE ASSASSIN");
         } else if (type == 4) {
             player.setHealth(50);
             player.setDamage(75);
             player.setLuck(.10);
             player.setWallet(225);
-            System.out.println("You chose NECROMANCER");
+            System.out.println("YOU CHOSE NECROMANCER");
         } else if (type == 5) {
             player.setHealth(50);
             player.setDamage(70);
