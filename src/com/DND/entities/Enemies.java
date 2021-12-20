2de1b6348498d1c7955fb1eccb5db18457e6b263
Vojtěch Černý <91047341+ProrokCZ13@users.noreply.github.com>
2021-12-20 22:12:36 +0100

    Update Enemies.java
    new enemies

--- src/com/DND/entities/Enemies.java
+++ src/com/DND/entities/Enemies.java
@@ -7,7 +7,11 @@ public enum Enemies {
     DIO(new Entity("DIO", 500, 45, 0.8)),
     TURRET(new Entity("TURRET", 120, 20, 0.4)),
     CRAB(new Entity("CRAB", 80, 20, 0.35)),
-    SLIME(new Entity("SLIME", 50, 15, 0.3));
+    SLIME(new Entity("SLIME", 50, 15, 0.3)),
+    CURSED_SOLDIER(new Entity("CURSED_SOLDIER", 50, 15, 0.3)),
+    CURSED_MAGE(new Entity("CURSED_MAGE", 50, 15, 0.3)),
+    GHOUL(new Entity("CURSED_MAGE", 50, 15, 0.3)),
+    SNAIL(new Entity("SNAIL", 50, 15, 0.3));
 
 
     private Random random = new Random();
