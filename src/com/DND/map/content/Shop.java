6eaa65f2a326532b64acab293d7d32db7646d9b9
Vojtěch Černý <91047341+ProrokCZ13@users.noreply.github.com>
2022-01-05 06:56:10 +0100

    Update Shop.java

--- src/com/DND/map/content/Shop.java
+++ src/com/DND/map/content/Shop.java
@@ -12,15 +12,15 @@ public class Shop {
 
     public Shop(Player player) {
         this.player = player;
-        System.out.println("\n\"WHAT UP ASSHOLE, WANT TO LOOK THROUHG MY SHIT?\" - SAID TRADER");
+        System.out.println("\n\"HELLO DEAR ADVENTURER, IS THERE SOMETHING YOU WANT?\" - SAID TRADER");
     }
 
     public void goShopping() {
         System.out.println(
                 "Your wallet : " + player.getWallet() + " COINS\n" +
                 " - (1) - NEW WEAPON (+15 DMG) : 100 COINS\n" +
-                " - (2) - HEAL   (+20 HP) : 75 COINS\n" +
-                " - (0) - exit\n"
+                " - (2) - HEAL (+20 HP) : 75 COINS\n" +
+                " - (0) - EXIT\n"
         );
         System.out.print("Choose: ");
         int choice = scanner.nextInt();
@@ -28,7 +28,7 @@ public class Shop {
             if (player.getWallet() >= 100) {
                 player.setWallet(player.getWallet() - 100);
                 player.setDamage(player.getDamage() + 15);
-                System.out.println("YOU'VE BOUGHT WEAPON UPGRADE");
+                System.out.println("YOU'VE NEW WEAPON");
                 System.out.println("+15 DMG (" + player.getDamage() + " DMG)\n");
                 goShopping();
             } else {
@@ -40,7 +40,7 @@ public class Shop {
             if (player.getWallet() >= 75) {
                 player.setWallet(player.getWallet() - 75);
                 player.setHealth(player.getHealth() + 20);
-                System.out.println("YOU'VE BOUGHT HEALTH POTION");
+                System.out.println("YOU'VE BOUGHT HEAL");
                 System.out.println("+20 HP (" + player.getHealth() + " HP)\n");
                 goShopping();
             } else {
@@ -49,7 +49,7 @@ public class Shop {
                 goShopping();
             }
         } else {
-            System.out.println("\n\"GO FUCK YOUR SELF!\" - SAID TRADER");
+            System.out.println("\n\"SEE YOU LATER!\" - SAID TRADER");
         }
     }
 
